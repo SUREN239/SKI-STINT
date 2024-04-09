7fb58e335717fa388984c706fcba623c530b2293
SUREN S <121883751+SUREN239@users.noreply.github.com>
2024-04-09 16:38:10 +0530

    Intial Backend Structure Added

---
+++ stint/src/main/java/com/stint/StintApplication.java
@@ -0,0 +1,13 @@
+package com.stint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StintApplication.class, args);
+    }
+
+}
---
+++ stint/src/test/java/com/stint/StintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
